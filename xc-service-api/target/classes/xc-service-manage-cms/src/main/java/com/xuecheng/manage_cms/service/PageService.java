c812c0f7a7cc300a17cdb9d3b44cb420a22e516a
admin <admin>
2020-05-16 14:02:25 +0800

    修改课程等级、课程状态数据字典的bug,增加了Restcontroller和RequestMapping注解

---
+++ xc-service-api/target/classes/xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
@@ -0,0 +1,172 @@
+package com.xuecheng.manage_cms.service;
+
+import com.xuecheng.framework.domain.cms.CmsPage;
+import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
+import com.xuecheng.framework.domain.cms.response.CmsCode;
+import com.xuecheng.framework.domain.cms.response.CmsPageResult;
+import com.xuecheng.framework.exception.ExceptionCast;
+import com.xuecheng.framework.model.response.CommonCode;
+import com.xuecheng.framework.model.response.QueryResponseResult;
+import com.xuecheng.framework.model.response.QueryResult;
+import com.xuecheng.framework.model.response.ResponseResult;
+import com.xuecheng.manage_cms.dao.CmsPageRepository;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ * @create 2018-09-12 18:32
+ **/
+@Service
+public class PageService {
+
+    @Autowired
+    CmsPageRepository cmsPageRepository;
+
+
+    /**
+     * 页面查询方法
+     *
+     * @param page             页码，从1开始记数
+     * @param size             每页记录数
+     * @param queryPageRequest 查询条件
+     * @return
+     */
+    public QueryResponseResult findList(int page, int size, QueryPageRequest queryPageRequest) {
+
+        if (queryPageRequest == null) {
+            queryPageRequest = new QueryPageRequest();
+        }
+        //条件匹配器
+        //页面名称模糊查询，需要自定义字符串的匹配器实现模糊查询
+        ExampleMatcher exampleMatcher = ExampleMatcher.matching().withMatcher("pageAliase", ExampleMatcher.GenericPropertyMatchers.contains());
+        //条件值
+        CmsPage cmsPage = new CmsPage();
+        //站点ID
+        if (StringUtils.isNotEmpty(queryPageRequest.getSiteId())) {
+            cmsPage.setSiteId(queryPageRequest.getSiteId());
+        }
+        //模板ID
+        if (StringUtils.isNotEmpty(queryPageRequest.getTemplateId())) {
+            cmsPage.setTemplateId(queryPageRequest.getTemplateId());
+        }
+        //页面别名
+        if (StringUtils.isNotEmpty(queryPageRequest.getPageAliase())) {
+            cmsPage.setPageAliase(queryPageRequest.getPageAliase());
+        }
+        //创建条件实列
+        Example<CmsPage> example = Example.of(cmsPage, exampleMatcher);
+
+        //分页参数
+        if (page <= 0) {
+            page = 1;
+        }
+        page = page - 1;
+        if (size <= 0) {
+            size = 10;
+        }
+        Pageable pageable = PageRequest.of(page, size);
+        //
+//        Page<CmsPage> all = cmsPageRepository.findAll(pageable);
+        Page<CmsPage> all = cmsPageRepository.findAll(example, pageable);
+        QueryResult queryResult = new QueryResult();
+        queryResult.setList(all.getContent());//数据列表
+        queryResult.setTotal(all.getTotalElements());//数据总记录数
+        QueryResponseResult queryResponseResult = new QueryResponseResult(CommonCode.SUCCESS, queryResult);
+        return queryResponseResult;
+    }
+
+    /**添加页面
+     *
+     * @param cmsPage
+     * @return
+     */
+    /*public CmsPageResult add(CmsPage cmsPage) {
+        CmsPage cmsPage1 = cmsPageRepository.findByPageNameAndSiteIdAndPageWebPath(cmsPage.getPageName(), cmsPage.getSiteId(), cmsPage.getPageWebPath());
+        if (cmsPage1 == null) {
+            cmsPage.setPageId(null);
+            cmsPageRepository.save(cmsPage);
+            //返回结果
+            CmsPageResult cmsPageResult = new CmsPageResult(CommonCode.SUCCESS, cmsPage);
+            return cmsPageResult;
+        }
+        return new CmsPageResult(CommonCode.FAIL, null);
+    }*/
+
+    /**
+     * 添加页面
+     * @param cmsPage
+     * @return
+     */
+    public CmsPageResult add(CmsPage cmsPage){
+        CmsPage cmsPage1 = cmsPageRepository.findByPageNameAndSiteIdAndPageWebPath(cmsPage.getPageName(), cmsPage.getSiteId(), cmsPage.getPageWebPath());
+        if(cmsPage1!=null) {
+            ExceptionCast.cast(CmsCode.CMS_ADDPAGE_EXISTSNAME);
+        }
+
+        //条用dao层保存页面
+        cmsPage.setPageId(null);
+        cmsPageRepository.save(cmsPage);
+        return new CmsPageResult(CommonCode.SUCCESS,cmsPage);
+
+    }
+
+
+    /**根据id查询页面*/
+    public CmsPage getById(String id) {
+        Optional<CmsPage> optional = cmsPageRepository.findById(id);
+        if (optional.isPresent()) {
+            return optional.get();
+        }
+        return null;
+    }
+
+    /**跟新页面信息
+     *
+     * @param id
+     * @param cmsPage
+     * @return
+     */
+    public CmsPageResult update(String id, CmsPage cmsPage) {
+        //根据页面id查询信息
+        CmsPage one = this.getById(id);
+        if (one != null) {
+            one.setTemplateId(cmsPage.getTemplateId());
+            one.setSiteId(cmsPage.getSiteId());
+            one.setPageAliase(cmsPage.getPageAliase());
+            one.setPageName(cmsPage.getPageName());
+            one.setPageWebPath(cmsPage.getPageWebPath());
+            one.setPagePhysicalPath(cmsPage.getPagePhysicalPath());
+            //执行更新
+            CmsPage save = cmsPageRepository.save(one);
+            if (save != null) {
+                CmsPageResult cmsPageResult = new CmsPageResult(CommonCode.SUCCESS, save);
+                return cmsPageResult;
+            }
+        }
+
+        return new CmsPageResult(CommonCode.FAIL, null);
+    }
+
+    /**删除页面
+     *
+     * @param id
+     * @return
+     */
+    public ResponseResult delete(String id){
+        CmsPage one=this.getById(id);
+        if(one!=null)
+        {
+            cmsPageRepository.deleteById(id);
+            return new ResponseResult(CommonCode.SUCCESS);
+        }
+        return new ResponseResult(CommonCode.FAIL);
+
+    }
+
+}
--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/controller/SysDictionaryController.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/controller/SysDictionaryController.java
@@ -6,7 +6,11 @@ import com.xuecheng.manage_cms.service.SysdictionaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/sys/dictionary")
 public class SysDictionaryController implements SysDicthinaryControllerApi {
     @Autowired
     SysdictionaryService sysdictionaryService;
